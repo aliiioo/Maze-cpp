6521feb91978e159e3917e7b2504e7dcdf2c7c17
aliiioo <78133282+aliiioo@users.noreply.github.com>
2021-04-24 16:46:03 +0430

    Update hezato.cpp

--- hezato.cpp
+++ hezato.cpp
@@ -1,5 +1,5 @@
 // hezato.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+////Ali Ranjbaran****************************************************************************
 #include<cstdio>
 #include <iostream>
 
